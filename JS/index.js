2edb6f02ea3bbb1223eca3427558d06206babe31
AEdwinGnanaOli <edwina8677@gmail.com>
2026-06-10 22:07:16 +0530

    yes

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+document
+.getElementById("contactForm")
+.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    alert("✅ Thank you! Your message has been sent successfully.");
+
+    this.reset();
+});
